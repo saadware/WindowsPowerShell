75bf5075d8bdd69ab0251e90d42cd7b2e03822ba
Scott Saad <saadsj@gmail.com>
2011-11-03 16:59:27 -0600

    Fixed environemnt problems with not having BDS defined for msbuild. Worked sometimes but only when lucky. This will hopefully resolve that.

--- scripts/Invoke-CppBuilderBuild.ps1
+++ scripts/Invoke-CppBuilderBuild.ps1
@@ -14,6 +14,9 @@ Build target like 'Make' or 'Build' or 'Clean'
 .PARAMETER Config
 Build configuration to use like 'Debug' or 'Debug Build' or 'Release', etc.
 
+.PARAMETER TwineTargetFile
+The msbuild target file for TwineCompile
+
 .PARAMETER WarningAsError
 Enables the build option to treat any compile warnings as build errors.
 
@@ -38,103 +41,116 @@ None.
 
 [CmdletBinding()]
 param(
-	[Parameter(Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
-	[Alias("PSPath")]
-	[string[]]$Path,
-	[Parameter(Position=1)]
-	$Target = "Make",
-	[Parameter(Position=2)]
-	$Config = "Debug Build",
-	[switch]$WarningsAsError,
-	[switch]$Rebuild,
-	[switch]$Diagnose,
-	[switch]$Quiet,
-	[switch]$EnableTwine,
-	$TwineTargetFile = "C:\Program Files (x86)\JomiTech\TwineCompile\TCTargetsXE.targets"
+		[Parameter(Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+		[Alias("PSPath")]
+		[string[]]$Path,
+		[Parameter(Position=1)]
+		$Target = "Make",
+		[Parameter(Position=2)]
+		$Config = "Debug Build",
+		$TwineTargetFile = "C:\Program Files (x86)\JomiTech\TwineCompile\TCTargetsXE.targets",
+		[switch]$WarningsAsError,
+		[switch]$Rebuild,
+		[switch]$Diagnose,
+		[switch]$Quiet,
+		[switch]$EnableTwine
      )
 Begin
 {
-    Write-Verbose "Starting..."
+	Write-Verbose "Starting..."
 
-# stop on any errors
-	$ErrorActionPreference = "Stop"
+	# stop on any errors
+	#$ErrorActionPreference = "Stop"
 
-# enable debug if requested
+	# enable debug if requested
 	if ( $Debug )
 	{
-	    $DebugPreference = "Continue"
+		$DebugPreference = "Continue"
 	}
 
-# if twine is enabled, ensure the target script exists
-    if ( $EnableTwine -and -not (Test-Path $TwineTargetFile) )
-    {
-	Write-Error ("Invalid twine target file $TwineTargetFile: Specify with TwineTargetFile parameter.")
-    }
+	# if twine is enabled, ensure the target script exists
+	if ( $EnableTwine -and -not (Test-Path $TwineTargetFile) )
+	{
+		Write-Error ("Invalid twine target file $(TwineTargetFile): Specify with TwineTargetFile parameter.")
+	}
 
-# setup environment/pathing
-    Set-Item env:FrameworkDir "C:\Windows\Microsoft.NET\Framework\v2.0.50727"
+	# setup environment/pathing
+	if ( -not (Test-Path env:BDS) )
+	{
+		$bds = Get-Command bds.exe -ErrorAction SilentlyContinue
+		if ( $bds -eq $null )
+		{
+			throw "bds.exe could not be found in your path. Ensure it is."
+		}
+		Set-Item env:BDS (Get-item $bds.Path).Directory.Parent.FullName
+		Set-Item env:CG_BOOST_ROOT (join-path (Get-item $bds.Path).Directory.Parent.FullName 'include\boost_1_39')
+	}
+	Set-Item env:FrameworkDir "C:\Windows\Microsoft.NET\Framework\v2.0.50727"
 	Set-Item env:FrameworkVersion "v2.0.50727"
 	if ( (Get-Command msbuild.exe -ErrorAction SilentlyContinue) -eq $null )
 	{
-	    Set-Item env:Path "$env:FrameworkDir;$env:Path"
+		Set-Item env:Path "$env:FrameworkDir;$env:Path"
 	}
 }
 Process
 {
-    function Build-Project
-    {
-	Process
+	function Build-Project
 	{
-	    $fileObject = $_
-	    $projectPath = $fileObject.FullName
-	    $tmpBuildFile = $projectPath 
-	    Write-Verbose $projectPath
-	    $twineOptions = ""
-	    if ( $EnableTwine )
-	    {
-		Write-Verbose "Twine build enabled..."
-		$twineOptions = "/p:CoreCompileDependsOnTargets=`"RidlCompile;PasCompile;TCBuildFileList;TCCompile;AsmCompile;RcCompile`""
-# generate temporary project file that has the twine options punched in
-		$tmpBuildFile = -join ($projectPath + ".tmp")
+		Process
+		{
+			$fileObject = $_
+			$projectPath = $fileObject.FullName
+			$tmpBuildFile = $projectPath 
+			Write-Verbose $projectPath
+			$twineOptions = ""
+			if ( $EnableTwine )
+			{
+				Write-Verbose "Twine build enabled..."
+				$twineOptions = "/p:CoreCompileDependsOnTargets=`"RidlCompile;PasCompile;TCBuildFileList;TCCompile;AsmCompile;RcCompile`""
 
-		Write-Verbose "adding twine msbuild target to project"
-		[xml]$projXml = Get-Content $projectPath
-		$projXml.Project.InnerXml = ($projXml.Project.InnerXml + "<Import Project=`'$TwineTargetFile`' />")
-		$projXml.OuterXml | Out-File -FilePath $tmpBuildFile -Encoding UTF8
-	    }
+				# generate temporary project file that has the twine options punched in
+				$tmpBuildFile = -join ($projectPath + ".tmp")
 
-	    # Run cmd so that options get sent it properly.
-	    $theFileOutputName = $fileObject.BaseName
-	    cmd.exe /c "msbuild.exe /nologo $tmpBuildFile $twineOptions /t:$Target /p:BCC_WarningIsError=$WarningsAsError /p:OutputName=$theFileOutputName $(if ( $Diagnose ) {'/verbosity:diagnostic' } elseif( $VerbosePreference -eq 'Continue' ) { '/verbosity:detailed' } elseif ( $Quiet ) { '/verbosity:quiet' } else { '/verbosity:normal' }) /p:ForceRebuild=$Rebuild"
-	    if ( $LASTEXITCODE -ne 0 )
-	    {
-		throw "Failed to compile $projectPath..."
-	    }
+				Write-Verbose "adding twine msbuild target to project"
+				[xml]$projXml = Get-Content $projectPath
+				$projXml.Project.InnerXml = ($projXml.Project.InnerXml + "<Import Project=`'$TwineTargetFile`' />")
+				$projXml.OuterXml | Out-File -FilePath $tmpBuildFile -Encoding UTF8
+			}
+
+			# Run cmd so that options get sent it properly.
+			$theFileOutputName = $fileObject.BaseName
+			cmd.exe /c "msbuild.exe /nologo $tmpBuildFile $twineOptions /t:$Target /p:BCC_WarningIsError=$WarningsAsError /p:OutputName=$theFileOutputName $(if ( $Diagnose ) {'/verbosity:diagnostic' } elseif( $VerbosePreference -eq 'Continue' ) { '/verbosity:detailed' } elseif ( $Quiet ) { '/verbosity:quiet' } else { '/verbosity:normal' }) /p:ForceRebuild=$Rebuild"
+			if ( $LASTEXITCODE -ne 0 )
+			{
+				throw "Failed to compile $projectPath..."
+			}
+		}
+	} # function Build-Project
+
+	$file = $_
+	Write-Verbose $file.FullName
+	if ( $file.Extension -eq ".groupproj" )
+	{
+		# A group project contains multiple projects inside of it. Build each one.
+		Write-Verbose "processing project group..."
+		[xml]$groups = Get-Content $file
+		Push-Location $file.Directory
+		($groups.Project.ItemGroup | ? { $_.Projects -ne $null }).Projects | 
+			Select-Object -Property @{Name="Path"; Expression = {$file.Directory.FullName}},@{Name="ChildPath"; Expression = {$_.Include}} | 
+				Join-Path -Resolve | Get-Item | Build-Project
+		Pop-Location
 	}
-    } # function Build-Project
-
-    $file = $_
-    Write-Verbose $file.FullName
-    if ( $file.Extension -eq ".groupproj" )
-    {
-	# A group project contains multiple projects inside of it. Build each one.
-	Write-Verbose "processing project group..."
-	[xml]$groups = Get-Content $file
-	Push-Location $file.Directory
-	($groups.Project.ItemGroup | ? { $_.Projects -ne $null }).Projects | Select-Object -Property @{Name="Path"; Expression = {$file.Directory.FullName}},@{Name="ChildPath"; Expression = {$_.Include}} | Join-Path -Resolve | Get-Item | Build-Project
-	Pop-Location
-    }
-    elseif( $file.Extension -eq ".cbproj" )
-    {
-	Write-Verbose "processing project..."
-	$file | Build-Project
-    }
-    else
-    {
-	Write-Error ("invalid project file " + $file.FullName)
-    }
+	elseif( $file.Extension -eq ".cbproj" )
+	{
+		Write-Verbose "processing project..."
+		$file | Build-Project
+	}
+	else
+	{
+		Write-Error ("invalid project file " + $file.FullName)
+	}
 }
 End
 {
-    Write-Verbose "All Done"
+	Write-Verbose "All Done"
 }
