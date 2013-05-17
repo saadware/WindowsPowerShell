75dc254b265fb4589b1643a76073c7661665a2bb
Scott Saad <saadsj@gmail.com>
2013-05-17 10:26:22 -0600

    Added better support for multiple XE versions.
    Added support by name (i.e. XE, XE2, XE3, etc) for building with
    c++builder. Also, absorbed support for Twine a bit better. There are
    currently known issues with TwineCompile not working in XE3 from
    msbuild.

--- scripts/Invoke-CppBuilderBuild.ps1
+++ scripts/Invoke-CppBuilderBuild.ps1
@@ -14,11 +14,8 @@ Build target like 'Make' or 'Build' or 'Clean'
 .PARAMETER Config
 Build configuration to use like 'Debug' or 'Debug Build' or 'Release', etc.
 
-.PARAMETER TwineTargetFile
-The msbuild target file for TwineCompile
-
 .PARAMETER BDSVersion
-The version of BDS installed (default to '15.0').
+The version of BDS installed (default to XE)
 
 .PARAMETER WarningAsError
 Enables the build option to treat any compile warnings as build errors.
@@ -51,8 +48,7 @@ param(
 		$Target = "Make",
 		[Parameter(Position=2)]
 		$Config = "Debug Build",
-		$TwineTargetFile = "C:\Program Files (x86)\JomiTech\TwineCompile\TCTargetsXE.targets",
-		$BDSVersion = "15.0",
+		$BDSVersion = "XE",
 		[switch]$WarningsAsError,
 		[switch]$Rebuild,
 		[switch]$Diagnose,
@@ -72,27 +68,52 @@ Begin
 		$DebugPreference = "Continue"
 	}
 
-	# if twine is enabled, ensure the target script exists
-	if ( $EnableTwine -and -not (Test-Path $TwineTargetFile) )
+	# attempt to see if we can find it in program files
+	$programFilesDir = $Env:ProgramFiles
+	if ( Test-Path 'Env:\ProgramFiles(x86)' )
 	{
-		Write-Warning "Invalid twine target file $($TwineTargetFile): disabling twine option"
-		$EnableTwine = $false
+		$programFilesDir = ${env:ProgramFiles(x86)} 
+	}
+
+	# Find the msbuild target file for twine
+	if ( $EnableTwine )
+	{
+		$TwineTargetFile = ls $programFilesDir -Recurse -Filter TCTargets*.targets -ErrorAction SilentlyContinue | ? {
+			$_.BaseName.EndsWith( $BDSVersion )
+		}
+
+		if ( $TwineTargetFile -eq $null )
+		{
+			Write-Warning "Invalid twine target file $($TwineTargetFile.FullName): disabling twine option"
+			$EnableTwine = $false
+		}
+		else
+		{
+			Write-Verbose "Found twine target file $($TwineTargetFile.FullName)."
+		}
+	}
+	else
+	{
+		Write-Verbose "Twine disabled."
 	}
 
 	# setup environment/pathing
 	if ( -not (Test-Path env:BDS) )
 	{
+		$bdsVersionNumber = "15.0"
+		switch ( $BDSVersion )
+		{
+			"XE"  { $bdsVersionNumber = "15.0"; break }
+			"XE2" { $bdsVersionNumber = "16.0"; break }
+			"XE3" { $bdsVersionNumber = "17.0"; break }
+		}
+		Write-Verbose "Found BDS version $bdsVersionNumber."
+
 		# get bds that matches our version (probably in path)
-		$bds = Get-Command bds.exe -ErrorAction SilentlyContinue | ? { $_.FileVersionInfo.ProductVersion -eq $BDSVersion }
+		$bds = Get-Command bds.exe -ErrorAction SilentlyContinue | ? { $_.FileVersionInfo.ProductVersion -eq $bdsVersionNumber }
 		if ( $bds -eq $null )
 		{
-			# attempt to see if we can find it in program files
-			$programFilesDir = $Env:ProgramFiles
-			if ( Test-Path 'Env:\ProgramFiles(x86)' )
-			{
-				$programFilesDir = ${env:ProgramFiles(x86)} 
-			}
-			$bds = ls -Filter bds.exe -Path (join-path $programFilesDir 'Embarcadero') -Recurse | ? { $_.VersionInfo.ProductVersion -eq $BDSVersion }
+			$bds = ls -Filter bds.exe -Path (join-path $programFilesDir 'Embarcadero') -Recurse | ? { $_.VersionInfo.ProductVersion -eq $bdsVersionNumber }
 			if ( $bds -eq $null )
 			{
 				throw "bds.exe could not be found in your path. Ensure it is."
@@ -104,9 +125,20 @@ Begin
 		}
 		Set-Item env:BDS $bds.Directory.Parent.FullName
 		Set-Item env:CG_BOOST_ROOT (join-path $bds.Directory.Parent.FullName 'include\boost_1_39')
+		write-verbose "Using bds: $($bds.FullName)"
 	}
-	Set-Item env:FrameworkDir "C:\Windows\Microsoft.NET\Framework\v2.0.50727"
-	Set-Item env:FrameworkVersion "v2.0.50727"
+	if ( $BDSVersion -eq "XE3" )
+	{
+		Set-Item env:FrameworkDir "C:\Windows\Microsoft.NET\Framework\v3.5"
+		Set-Item env:FrameworkVersion "v3.5"
+		Set-Item env:FrameworkSDKDir ""
+
+	}
+	else
+	{
+		Set-Item env:FrameworkDir "C:\Windows\Microsoft.NET\Framework\v2.0.50727"
+		Set-Item env:FrameworkVersion "v2.0.50727"
+	}
 	if ( (Get-Command msbuild.exe -ErrorAction SilentlyContinue).Path -ne "$env:FrameworkDir\MSBuild.exe")
 	{
 		Set-Item env:Path "$env:FrameworkDir;$env:Path"
@@ -126,7 +158,7 @@ Process
 			if ( $EnableTwine )
 			{
 				Write-Verbose "Twine build enabled..."
-				$twineOptions = "/p:CoreCompileDependsOnTargets=`"RidlCompile;PasCompile;TCBuildFileList;TCCompile;AsmCompile;RcCompile`""
+				#$twineOptions = "/p:CoreCompileDependsOnTargets=`"RidlCompile;PasCompile;TCBuildFileList;TCCompile;AsmCompile;RcCompile`""
 
 				# generate temporary project file that has the twine options punched in
 				$tmpBuildFile = -join ($projectPath + ".tmp")
@@ -136,12 +168,12 @@ Process
 
 				Write-Verbose "adding twine msbuild target to project"
 				[xml]$projXml = Get-Content $projectPath
-				$projXml.Project.InnerXml = ($projXml.Project.InnerXml + "<Import Project=`'$TwineTargetFile`' />")
+				$projXml.Project.InnerXml = ($projXml.Project.InnerXml + "<Import Project=`'$($TwineTargetFile.FullName)`' />")
 				$projXml.OuterXml | Out-File -FilePath $tmpBuildFile -Encoding UTF8
 			}
 			# Run cmd so that options get sent it properly.
 			$theFileOutputName = $fileObject.BaseName
-			cmd.exe /c "msbuild.exe /nologo $tmpBuildFile $twineOptions /t:$Target /p:Config=`"$Config`" /p:BCC_WarningIsError=$WarningsAsError /p:OutputName=$theFileOutputName $(if ( $Diagnose ) {'/verbosity:diagnostic' } elseif( $VerbosePreference -eq 'Continue' ) { '/verbosity:detailed' } elseif ( $Quiet ) { '/verbosity:quiet' } else { '/verbosity:normal' }) /p:ForceRebuild=$Rebuild"
+			cmd.exe /c "msbuild.exe /nologo $tmpBuildFile $twineOptions /t:$Target /p:Platform=`"Win32`" /p:Config=`"$Config`" /p:BCC_WarningIsError=$WarningsAsError /p:OutputName=$theFileOutputName $(if ( $Diagnose ) {'/verbosity:diagnostic' } elseif( $VerbosePreference -eq 'Continue' ) { '/verbosity:detailed' } elseif ( $Quiet ) { '/verbosity:quiet' } else { '/verbosity:normal' }) /p:ForceRebuild=$Rebuild"
 			if ( $LASTEXITCODE -ne 0 )
 			{
 				throw "Failed to compile $projectPath..."
